37f8996c7914d4483948e76799340ecdf47b660c
Nikki Mallory <nikkimallory13@gmail.com>
2021-06-03 16:30:28 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+        //ROUTES//
+
+app.get("/bugets", (req, res)=>{
+    res.send(budgets)
+});
+
+        //Show//
+
+app.get("/budgets/:index", (req, res)=>{
+    res.send(indexOfBudgets)
+});
+
+        //New//
+
+app.get("/bugets/:new", (req, res)=>{
+    res.send(newBudgets)
+});
+
+        //Create//
+
+app.post("/budgets", (req, res)=>{
+    res.send()
+});
+
+        //Web Server//
+app.listen(port, ()=>{
+    console.log("Express is listening on port ", port);
+});
