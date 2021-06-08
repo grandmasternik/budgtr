d2512a40293bcdb0c44e190ebaae1e5938135fed
Nikki Mallory <nikkimallory13@gmail.com>
2021-06-07 20:28:55 -0500

    file structured updated

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
-    // Variables//
+    //Dependencies//
 const express = require('express');
 
 const budget = require("./models/budget.js");
-
 const app = express();
-
 const port = 3000;
-        // Imports//
-
 const models = require('./models/budget.js')
 
-        //ROUTES//
+        //index//
 app.get("/bugets", (req, res)=>{
     res.send(budgets)
 });
 
         //Show//
-
 app.get("/budgets/:index", (req, res)=>{
     res.send(indexOfBudgets)
 });
 
         //New//
-
 app.get("/bugets/:new", (req, res)=>{
     res.send(newBudgets)
 });
 
         //Create//
-
 app.post("/budgets", (req, res)=>{
     res.send()
 });
