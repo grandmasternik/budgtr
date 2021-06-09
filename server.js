d9e533a4cfbd6d7149281b59cd5f3624f53ae09f
Nikki Mallory <nikkimallory13@gmail.com>
2021-06-09 01:43:34 -0500

    new.ejs updated and body parsing

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const budget = require("./models/budget.js");
+const bodyParser = require(`body-parser`);
 
 // static config//
 // app.use(express.urlencoded({ extended: true }));
